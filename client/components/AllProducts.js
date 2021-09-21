5d2170846944462dffd937b4b55cbc19485fac48
Ashley Williams <ashleyswilliams01@gmail.com>
2021-09-21 16:09:44 -0600

    all products rendering in mui cards

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchProducts } from '../store/products'
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const useStyles = theme => ({
+  root: {
+    maxWidth: 345,
+    margin: 'auto'
+  },
+  media: {
+    height: '100%',
+    width: '100%'
+  }
+})
 
 export class AllProducts extends React.Component {
   constructor() {
@@ -13,9 +32,36 @@ export class AllProducts extends React.Component {
   }
 
   render() {
+    console.log(this.props.products)
+    const {classes} = this.props;
     return (
-      //will need to map over this.props.products and render out cards
-      <h1>All Products Page</h1>
+      <div>
+        <Grid container>
+          {this.props.products.map(plant => (
+            <Grid item key={plant.id} xs={12} md={6} lg={4}>
+              <Card className={classes.root}>
+                <CardMedia
+                  className={classes.media}
+                  component="img"
+                  image={plant.imageUrl}
+                  alt={plant.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {plant.name}
+                  </Typography>
+                  <Typography gutterBottom variant="h6" component="div">
+                    Price: {plant.price}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     )
   }
 }
@@ -32,4 +78,7 @@ const mapDispatch = (dispatch) => {
   }
 }
 
-export default connect(mapState, mapDispatch)(AllProducts);
+export default connect(
+  mapState,
+  mapDispatch)
+  (withStyles(useStyles)(AllProducts));
