3d7fc815b0d80d3bb139cbad1957de22e8d22f50
Helena Bliss <helenajbliss75@gmail.com>
2021-09-21 11:39:46 -0500

    DB Models in progress

---
+++ server/db/models/Product.js
@@ -0,0 +1,29 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Product = db.define("product", {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  price: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.TEXT,
+  },
+});
+
+module.exports = Product;
