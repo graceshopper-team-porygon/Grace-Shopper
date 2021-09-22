8d6d78eca143dd4cffc091db01016bf7d2f0555b
Nicole Buendia <70274300+nbuendia@users.noreply.github.com>
2021-09-22 14:44:09 -0400

    added materialUI-icons, changed single product component, still in progress

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,7 +1,34 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import { Link } from "react-router-dom";
 import { fetchSingleProduct } from "../store/singleProduct";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+  Grid,
+  Paper,
+  withStyles,
+} from "@material-ui/core";
+import { KeyboardArrowLeft, AddShoppingCart } from "@material-ui/icons";
+
+const useStyles = (theme) => ({
+  root: {
+    maxWidth: "100%",
+    maxHeight: 645,
+    margin: "auto",
+    "align-content": "center",
+  },
+  media: {
+    height: "100%",
+    width: 200,
+    "border-top-left-radius": "10px",
+    "border-bottom-right-radius": "10px",
+  },
+});
 
 class SingleProduct extends Component {
   componentDidMount() {
@@ -10,10 +37,48 @@ class SingleProduct extends Component {
   }
 
   render() {
+    console.log("PROPS", this.props);
+    const { classes, product } = this.props;
+
     return (
       <div>
-        <h1>SINGLE PRODUCT</h1>
-        <p></p>
+        <Card className={classes.root}>
+          <CardMedia
+            className={classes.media}
+            component="img"
+            image={product.imageUrl}
+            alt={product.name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {product.name}
+            </Typography>
+            <Typography gutterBottom variant="subtitle1" component="div">
+              {product.description}
+            </Typography>
+            <br />
+            <Typography gutterBottom variant="body1" component="div">
+              Price: ${product.price}
+              <br />
+              {product.quantity <= 3 && product.quantity > 0
+                ? `Only ${product.quantity} in Stock!`
+                : product.quantity === 0
+                ? "Out of Stock"
+                : "In Stock"}
+            </Typography>
+          </CardContent>
+
+          <CardActions>
+            <Link to={"/products"}>
+              <Button startIcon={<KeyboardArrowLeft />} size="small">
+                All Products
+              </Button>
+            </Link>
+            <Button endIcon={<AddShoppingCart />} size="small">
+              Add To Cart
+            </Button>
+          </CardActions>
+        </Card>
       </div>
     );
   }
@@ -31,4 +96,7 @@ const mapDispatch = (dispatch) => {
   };
 };
 
-export default connect(mapState, mapDispatch)(SingleProduct);
+export default connect(
+  mapState,
+  mapDispatch
+)(withStyles(useStyles)(SingleProduct));
