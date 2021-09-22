327b1449d74a6e6a461207901adedaa58401562f
Duchess <veraruthkahn@gmail.com>
2021-09-22 12:02:07 -0400

    basic styling on cart

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,14 +1,14 @@
 import { connect } from "react-redux";
 import { getCartItems } from "../store/cartItems";
-import React from 'react'
-//import * as React from 'react';
-// import Table from '@mui/material/Table';
-// import TableBody from '@mui/material/TableBody';
-// import TableCell from '@mui/material/TableCell';
-// import TableContainer from '@mui/material/TableContainer';
-// import TableHead from '@mui/material/TableHead';
-// import TableRow from '@mui/material/TableRow';
-// import Paper from '@mui/material/Paper';
+//import React from 'react'
+import * as React from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 //get cartItems thunk
 //render all the items
@@ -26,14 +26,28 @@ class Cart extends React.Component {
     if (!this.state.didFetch) return <h1>Loading...</h1>;
     else if (this.props.cartItems.length === 0)
       return <h1>Time to go shopping</h1>; //make this a link back to products
-    return this.props.cartItems.map((item, i) => {
-      return (
-        <div key={i + item.id} className="item-row">
-          <div className="item-name">{item.product.name}</div>
-          <div className="item-total-price">{item.product.price * item.quantity}</div>
-        </div>
-      );
-    });
+    return (
+      <TableContainer component={Paper}>
+          <Table sx = {{minWidth:650}} aria-label="simple table">
+              <TableHead>
+                  <TableRow>
+                      <TableCell>Product</TableCell>
+                      <TableCell align = "right">Quantity</TableCell>
+                      <TableCell align = "right">Total Cost</TableCell>
+                  </TableRow>
+              </TableHead>
+              <TableBody>
+                  {this.props.cartItems.map(item=>(
+                      <TableRow key= {item.product.name} sx={{'&:last-child td, &:last-child th':{border:0}}}>
+                          <TableCell component = 'th'scope = 'row'>{item.product.name}</TableCell>
+                          <TableCell align="right">{item.quantity}</TableCell>
+                          <TableCell align = "right">{(item.quantity*item.product.price).toFixed(2)}</TableCell>
+                      </TableRow>
+                  ))}
+              </TableBody>
+        </Table>
+      </TableContainer>
+    );
   }
 }
 
