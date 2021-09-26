414562c5dad05d9863c68183b76ec34fb10c3dc7
Nicole Buendia <70274300+nbuendia@users.noreply.github.com>
2021-09-26 12:02:04 -0400

    changed styling

--- client/components/AddProduct.js
+++ client/components/AddProduct.js
@@ -2,6 +2,9 @@ import React from "react";
 import { connect } from "react-redux";
 import { createProduct } from "../store/products";
 
+import { Button } from "@material-ui/core";
+import { AddBoxOutlined } from "@material-ui/icons";
+
 export class AddProduct extends React.Component {
   constructor() {
     super();
@@ -11,6 +14,7 @@ export class AddProduct extends React.Component {
       quantity: "",
       description: "",
     };
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -29,50 +33,56 @@ export class AddProduct extends React.Component {
   render() {
     const { name, price, quantity, description } = this.state;
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label htmlFor="product_name">
-              <small>Product Name</small>
-            </label>
-            <input
-              name="name"
-              type="text"
-              value={name}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <label>
-              <small>Price</small>
-            </label>
-            <input name="price" value={price} onChange={this.handleChange} />
-          </div>
-          <div>
-            <label>
-              <small>Quantity</small>
-            </label>
-            <input
-              name="quantity"
-              value={quantity}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <label>
-              <small>Description</small>
-            </label>
-            <textarea
-              name="description"
-              rows="4"
-              cols="40"
-              value={description}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <button type="submit">Add Product</button>
-          </div>
+      <div className="form-container">
+        <h4>ADD PRODUCT</h4>
+        <form onSubmit={this.handleSubmit} className="add-form">
+          <label htmlFor="product_name">
+            <small>Product Name</small>
+          </label>
+          <input
+            name="name"
+            type="text"
+            value={name}
+            onChange={this.handleChange}
+            className="input"
+          />
+          <label>
+            <small>Price</small>
+          </label>
+          <input
+            name="price"
+            value={price}
+            onChange={this.handleChange}
+            className="input"
+          />
+          <label>
+            <small>Quantity</small>
+          </label>
+          <input
+            name="quantity"
+            value={quantity}
+            onChange={this.handleChange}
+            className="input"
+          />
+          <label>
+            <small>Description</small>
+          </label>
+          <textarea
+            name="description"
+            value={description}
+            onChange={this.handleChange}
+            className={"text-box"}
+          />
+          <br />
+          {/* <button type="submit">Add Product</button> */}
+          <Button
+            variant="contained"
+            type="submit"
+            size="small"
+            endIcon={<AddBoxOutlined />}
+          >
+            Submit Changes
+          </Button>
         </form>
       </div>
     );
