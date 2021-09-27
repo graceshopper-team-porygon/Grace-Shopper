4fb4eff5d812c7f250fa5339dc970fa53f19c731
Duchess <veraruthkahn@gmail.com>
2021-09-27 12:33:11 -0400

    woops

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -141,7 +141,13 @@ class Cart extends React.Component {
                 ))}
                 <TableRow>
                   <TableCell />
-                  <TableCell></TableCell>
+                  <TableCell>
+                    <Link to="/checkout">
+                      <Button onClick={() => this.checkoutClickHandler()}>
+                        Checkout
+                      </Button>
+                    </Link>
+                  </TableCell>
                   <TableCell />
                   <TableCell align="right">
                     Total:
@@ -152,13 +158,6 @@ class Cart extends React.Component {
             )}
           </Table>
         </TableContainer>
-        <div>
-          <Link to="/checkout">
-            <Button onClick={() => this.checkoutClickHandler()}>
-              Checkout
-            </Button>
-          </Link>
-        </div>
       </div>
     );
   }
